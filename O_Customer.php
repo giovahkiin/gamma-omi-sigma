d5277eb959ce947d1f6fac778b4a26221de1d8b7
Gener Angelo Lopez <Gener Angelo Lopez>
2017-05-09 21:35:28 +0800

    revert O_Customer.php to version with basic SQL linking

--- O_Customer.php
+++ O_Customer.php
@@ -18,21 +18,14 @@
 			<h2> Customers </h2>
 			<input type = "text" placeholder = "search">
 		<div id = "Table">
+			<?php
+			 	include 'config.php';
+				$sqlresult = $conn->query("SELECT customer.customer_id AS 'Customer Number', customer.name AS 'Name', agent.name AS 'Agent', orders.order_no as 'Order Number', orders.amount_due AS 'Total Amount' FROM customer, agent, orders WHERE customer.agent_id = agent.agent_id AND customer.customer_id = orders.customer_id;");
+	            echo sql_to_html_table($sqlresult, $delim = "\n");
+				$conn->close();
+			 ?>
+
 			<table>
-				<tr> 
-					<td> Customer Number </td>
-					<td> Name </td>
-					<td> Agent </td>
-					<td> Order Number </td>
-					<td> Total Amount </td>
-				</tr>
-				<tr>
-					<td> (xxxx) <td>
-					<td> (name) </td>
-					<td> (name) </td>
-					<td> (xxxx) </td>
-					<td> (x) </td>
-				</tr>
 				<tr>
 					<th> Total Sales </th>
 					<td> (x) </td>
