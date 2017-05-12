a91b86cb7ca89d9a4a0bcc7e97ed4433c7eebd8f
Gio Lopez <gener.lopez@gmail.com>
2017-05-12 22:49:25 +0800

    My_Cart CSS also added

--- My_Cart.php
+++ My_Cart.php
@@ -2,48 +2,53 @@
 <html>
 	<head>
 		<title> Inventory </title>
+	    <link rel="stylesheet" type="text/css" href="My_Cart.css"/>
 	</head>
 	<body>
-		<div class = "container">
-			<div id = "Bar">
-                <ul>
-                    <li> <a href = "./Main_Menu.php"> Main Menu </a></li>
-                    <li> User Name Here </li>
-                    <li> <a href = "./Login.php"> Log Out </a></li>
+			<div class = "Bar">
+                <ul id = "BarList">
+                    <li id = "Name"> User Name Here </li>
+                    <li> <a href = "./Main_Menu.php"> Main Menu </a> </li>
+                    <li> <a href = "./Login.php"> Log Out </a> </li>
                 </ul>
-          	</div>
-		</div>
-		<div class = "Title">
+            </div>
+		<div class = "header">
+		<div id = "Title">
 			<input type = "text" placeholder = "search">
 			<h1> Inventory </h1>
 		</div>
-		<div class = "Table">
-			<table>
+		</div>
+		<div id = "Table">
+            
+			<table class = "center">
 				<tr> 
-					<th> Date </th>
-				</tr>
-				<tr> 
-					<td> Product </td>
-					<td> Type </td>
-					<td> Color </td>
-					<td> Personalization Options </td>
-					<td> Quantity </td>
-					<td> Discount </td>
-					<td> Total Amount </td>
+					<th> Product </th>
+					<th> Type </th>
+					<th> Color </th>
+					<th> Personalization</th>
+					<th> Quantity </th>
+					<th> Discount </th>
+					<th> Total Amount </th>
 				</tr>
 				<tr>
-					<td> Product 1 <td>
-					<td> (x) </td>
-					<td> (x) </td>
-					<td> (x) </td>
-					<td> (x) </td>
-					<td> (x) </td>
-					<td> (x) </td>
+					<td>Product1</td>
+					<td> x </td>
+					<td> x </td>
+					<td> x </td>
+					<td> x </td>
+                    <td> x </td>
+                    <td> x </td>
 				</tr>
+                <tfoot>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>Amount Due:</td>
+                    <td>Insert Price</td>
+                </tfoot>
 			</table>
 		</div>
-        <div class = "totalAmount">
-            <h2>Amount Due</h2>
-        </div>
 	</body>
 </html>
